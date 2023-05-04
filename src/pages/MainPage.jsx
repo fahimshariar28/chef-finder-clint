d277cd053c0ade2bd142c5f6550bf48b705b29a7
Fahim Shariar <fahimshariar28@gmail.com>
2023-05-04 15:09:54 +0600

    Contact Section Added

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../components/Banner";
 import Chefs from "../components/Chefs";
+import Contact from "../components/Contact";
 
 const MainPage = () => {
   return (
     <div className="w-9/12 mx-auto mt-5">
       <Banner></Banner>
       <Chefs></Chefs>
+      <Contact></Contact>
     </div>
   );
 };
